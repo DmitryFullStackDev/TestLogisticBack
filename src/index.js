2afe7b07de94763fca7a70cbce6e7da34e21c7d8
Dmitry Yushkevich <dmitryyushkevich@MacBook-Pro-Dmitry.local>
2023-02-27 22:56:41 +0300

    heroku

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const routes = require('./routes')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello server is running').end()
-})
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', routes)
+app.use(errorHandler)
 
-// Start the server
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-  console.log('Press Ctrl+C to quit.')
-})
+const start = async () => {
+  try {
+    app.listen(process.env.PORT, () => console.log('run ' + 8080))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
